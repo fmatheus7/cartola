586e2ee6ac46f6c1e58ee2951ea4422b498edf74
Matheus Freire <matheusfreire77@gmail.com>
2022-06-24 11:21:17 +0100

    changes on the ui

--- components/Confronts.jsx
+++ components/Confronts.jsx
@@ -4,20 +4,29 @@ const Confronts = ({ weekConfronts }) => {
   return (
     <div>
       {weekConfronts.map((confronts, idx) => (
-        <table
+        <div
           key={idx}
-          className=' border-collapse text-center mx-auto w-full'>
-          <thead className='font-bold'></thead>
-          <tbody className='bg-slate-800'>
-            <tr className='border-b-8 border-b-slate-500 h-14 w-32'>
-              <td className='text-left px-2'>{confronts.team1}</td>
-              <td className='text-right'>{confronts.score1}</td>
-              <td className=''>X</td>
-              <td className='text-left'>{confronts.score2}</td>
-              <td className='text-right px-2'>{confronts.team2}</td>
-            </tr>
-          </tbody>
-        </table>
+          className='flex items-center justify-between w-[320px] mx-auto border rounded bg-slate-600 py-4'>
+          <div>
+            <p className='text-2xl text-gray-50 p-2'>{confronts.team1}</p>
+          </div>
+          <div className='flex items-center justify-between '>
+            {/* Score */}
+            <div className=''>
+              <p className='font-bold px-4 '>{confronts.score1}</p>
+            </div>
+            <div>
+              <p className='font-bold px-2'>X</p>
+            </div>
+            <div>
+              <p className='font-bold px-4'>{confronts.score2}</p>
+            </div>
+            {/* Score */}
+          </div>
+          <div>
+            <p className='text-2xl text-gray-50 p-2'>{confronts.team2}</p>
+          </div>
+        </div>
       ))}
     </div>
   );
