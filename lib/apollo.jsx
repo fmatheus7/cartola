bf2c266a7523f2a700e6d01a3c247f6926c56193
Paulo <paulo.freire53@gmail.com>
2022-06-24 12:14:53 -0300

    change uri

--- lib/apollo.jsx
+++ lib/apollo.jsx
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
-  uri: "https://api-sa-east-1.graphcms.com/v2/cl4kdaq7j5o7t01xvgqpydj0g/master",
+  //uri: "https://api-sa-east-1.graphcms.com/v2/cl4kdaq7j5o7t01xvgqpydj0g/master",
+  uri: "https://jsonkeeper.com/b/K14E",
   cache: new InMemoryCache(),
 });
 
