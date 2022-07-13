442216d27640b7546ef34e5c45819a0791eb5e82
Paulo <paulo.freire53@gmail.com>
2022-07-12 22:16:17 -0300

    add logo image to header

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,18 +1,25 @@
 import Link from "next/link";
 import React from "react";
+import Image from "next/image";
+import logo from "../assets/imgs/ligaimg.png";
 
-const Navbar = () => {
+const Navbar = (props) => {
   return (
-    <div className='w-full h-20 bg-slate-600 py-4 shadow-2xl'>
-      <div className='flex items-center justify-between font-bold text-white py-2 mx-auto max-w-7xl '>
-        <h1 className='text-2xl whitespace-normal bold'>Cartola Confront</h1>
+    <nav class="flex items-center justify-between flex-wrap bg-teal p-6">
+      <div class="flex items-center flex-no-shrink text-white mr-6">
+        <Image src={logo} class="h-8 w-8 mr-2" width="64" height="64" />
+        <span class="font-semibold text-xl tracking-tight">
+          GAROTEANTEAMPIONS LEAGUE
+        </span>
         <div>
-          <Link href='/' className=''>
-            <h1 className='text-2xl whitespace-normal bold'>Tabela</h1>
+          <Link href="/" className="">
+            <h1 className="text-2xl whitespace-normal bold">
+              Desafio Bolas de ouro
+            </h1>
           </Link>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
